4c9bdd48f22a0e4b7b34ce1d93f53ba04979615a
khomych <tsirkuny@gmail.com>
2020-01-16 20:27:20 +0200

    Update script.js

--- script/script.js
+++ script/script.js
@@ -8,13 +8,19 @@ document.addEventListener('DOMContentLoaded', () => {
     const cartModal = document.querySelector('.cart');
     const btnCartClose = cartModal.querySelector('.cart-close');
 
+
+
+
+
+
+
     const createCardGoods = (id, title, price, img) => {
         const card = document.createElement('div');
         card.className = 'card-wrapper col-12 col-md-6 col-lg-4 col-xl-3 pb-3';
         card.innerHTML = `
         <div class="card">
             <div class="card-img-wrapper">
-                <img class="card-img-top" src="./img/temp/${img}" alt="">
+                <img class="card-img-top" src="${img}" alt="">
                 <button class="card-add-wishlist" data-giids-id = "${id}"></button>
             </div>
             <div class="card-body justify-content-between">
@@ -30,35 +36,41 @@ document.addEventListener('DOMContentLoaded', () => {
         return card;
     }
 
+    const renderCard = (item) => {
+        goodsWrapper.textContent = '';
+        item.forEach(({ id, title, price, imgMin }) => {
+            goodsWrapper.appendChild(createCardGoods(id, title, price, imgMin));
+        });
+    };
+
+    const getGoods = (handler) => {
+        fetch('db/db.json')
+            .then(response => response.json())
+            .then(handler);
+    };
+
     const openCart = (event) => {
         event.preventDefault();
+        document.addEventListener('keydown', closeCart);
         cartModal.style.display = 'flex';
     };
 
     const closeCart = (event) => {
         const target = event.target;
-        event.preventDefault();
-        if (target.classList.contains('cart') || target.classList.contains('cart-close')) {
+        if (target.classList.contains('cart') || target.classList.contains('cart-close') ||
+            event.keyCode === 27) {
             cartModal.style.display = 'none';
         }
+
+        document.removeEventListener('keydown', closeCart);
     };
 
 
-    goodsWrapper.appendChild(createCardGoods(0, 'test0', 1001, 'Flamingo.jpg'));
-    goodsWrapper.appendChild(createCardGoods(1, 'test1', 1002, 'Socks.jpg'));
-    goodsWrapper.appendChild(createCardGoods(2, 'test2', 1003, 'Socks.jpg'));
-    goodsWrapper.appendChild(createCardGoods(3, 'test3', 1004, 'Archer.jpg'));
-    goodsWrapper.appendChild(createCardGoods(4, 'test4', 1005, 'Flamingo.jpg'));
-    goodsWrapper.appendChild(createCardGoods(5, 'test5', 1006, 'Archer.jpg'));
-
     btnCart.addEventListener('click', openCart);
     cartModal.addEventListener('click', closeCart);
-    document.addEventListener('keydown', (event) => {
-        if (event.keyCode === 27) {
-            cartModal.style.display = 'none';
-            event.preventDefault();
-        }
-    });
+
+    getGoods(renderCard);
+
 
 
 
